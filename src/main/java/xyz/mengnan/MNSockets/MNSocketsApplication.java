4ad2a6179d3f44f07ef7b182b72b7e959ac513b7
aoebiu <59640283+aoebiu@users.noreply.github.com>
2020-11-13 11:48:48 +0800

    Update MNSocketsApplication.java

--- src/main/java/xyz/mengnan/MNSockets/MNSocketsApplication.java
+++ src/main/java/xyz/mengnan/MNSockets/MNSocketsApplication.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, aoebiu (ltl1510@126.com)
+ * Copyright (c) 2020, aoebiu (ltl1510@126.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
